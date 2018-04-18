44242a2093da9474ed48746103ed34cc4337dd8d
root <511392148@qq.com>
2018-04-18 09:45:19 +0800

    5 stage pipeline with forwarding, only under test instruction implementation

--- sim-pipe.c
+++ sim-pipe.c
@@ -31,7 +31,7 @@ sim_reg_options(struct opt_odb_t *odb)
 {
   opt_reg_header(odb, 
 "sim-pipe: This simulator implements based on sim-fast.\n"
-		 );
+     );
 }
 
 /* check simulator-specific option values */
@@ -48,16 +48,16 @@ sim_reg_stats(struct stat_sdb_t *sdb)
 {
 #ifndef NO_INSN_COUNT
   stat_reg_counter(sdb, "sim_num_insn",
-		   "total number of instructions executed",
-		   &sim_num_insn, sim_num_insn, NULL);
+       "total number of instructions executed",
+       &sim_num_insn, sim_num_insn, NULL);
 #endif /* !NO_INSN_COUNT */
   stat_reg_int(sdb, "sim_elapsed_time",
-	       "total simulation time in seconds",
-	       &sim_elapsed_time, 0, NULL);
+         "total simulation time in seconds",
+         &sim_elapsed_time, 0, NULL);
 #ifndef NO_INSN_COUNT
   stat_reg_formula(sdb, "sim_inst_rate",
-		   "simulation speed (in insts/sec)",
-		   "sim_num_insn / sim_elapsed_time", NULL);
+       "simulation speed (in insts/sec)",
+       "sim_num_insn / sim_elapsed_time", NULL);
 #endif /* !NO_INSN_COUNT */
   ld_reg_stats(sdb);
   mem_reg_stats(mem, sdb);
@@ -69,22 +69,22 @@ struct idex_buf de;
 struct exmem_buf em;
 struct memwb_buf mw;
 
-#define DNA			(-1)
+#define DNA     (-1)
 
 /* general register dependence decoders */
-#define DGPR(N)			(N)
-#define DGPR_D(N)		((N) &~1)
+#define DGPR(N)     (N)
+#define DGPR_D(N)   ((N) &~1)
 
 /* floating point register dependence decoders */
-#define DFPR_L(N)		(((N)+32)&~1)
-#define DFPR_F(N)		(((N)+32)&~1)
-#define DFPR_D(N)		(((N)+32)&~1)
+#define DFPR_L(N)   (((N)+32)&~1)
+#define DFPR_F(N)   (((N)+32)&~1)
+#define DFPR_D(N)   (((N)+32)&~1)
 
 /* miscellaneous register dependence decoders */
-#define DHI			(0+32+32)
-#define DLO			(1+32+32)
-#define DFCC		(2+32+32)
-#define DTMP		(3+32+32)
+#define DHI     (0+32+32)
+#define DLO     (1+32+32)
+#define DFCC    (2+32+32)
+#define DTMP    (3+32+32)
 
 /* initialize the simulator */
 void
@@ -111,9 +111,9 @@ sim_init(void)
 
 /* load program into simulated state */
 void
-sim_load_prog(char *fname,		/* program to load */
-	      int argc, char **argv,	/* program arguments */
-	      char **envp)		/* program environment */
+sim_load_prog(char *fname,    /* program to load */
+        int argc, char **argv,  /* program arguments */
+        char **envp)    /* program environment */
 {
   /* load program text and data, set up environment, memory, and regs */
   ld_load_prog(fname, argc, argv, envp, &regs, mem, TRUE);
@@ -123,7 +123,7 @@ sim_load_prog(char *fname,		/* program to load */
 void
 sim_aux_config(FILE *stream)
 {  
-	/* nothing currently */
+  /* nothing currently */
 }
 
 /* dump simulator-specific auxiliary simulator statistics */
@@ -142,65 +142,65 @@ sim_uninit(void)
  */
 
 /* next program counter */
-#define SET_NPC(EXPR)		(regs.regs_NPC = (EXPR))
+#define SET_NPC(EXPR)   (regs.regs_NPC = (EXPR))
 
 /* current program counter */
-#define CPC			(regs.regs_PC)
+#define CPC     (regs.regs_PC)
 
 /* general purpose registers */
-#define GPR(N)			(regs.regs_R[N])
-#define SET_GPR(N,EXPR)		(regs.regs_R[N] = (EXPR))
-#define DECLARE_FAULT(EXP) 	{;}
+#define GPR(N)      (regs.regs_R[N])
+#define SET_GPR(N,EXPR)   (regs.regs_R[N] = (EXPR))
+#define DECLARE_FAULT(EXP)  {;}
 #if defined(TARGET_PISA)
 
 /* floating point registers, L->word, F->single-prec, D->double-prec */
-#define FPR_L(N)		(regs.regs_F.l[(N)])
-#define SET_FPR_L(N,EXPR)	(regs.regs_F.l[(N)] = (EXPR))
-#define FPR_F(N)		(regs.regs_F.f[(N)])
-#define SET_FPR_F(N,EXPR)	(regs.regs_F.f[(N)] = (EXPR))
-#define FPR_D(N)		(regs.regs_F.d[(N) >> 1])
-#define SET_FPR_D(N,EXPR)	(regs.regs_F.d[(N) >> 1] = (EXPR))
+#define FPR_L(N)    (regs.regs_F.l[(N)])
+#define SET_FPR_L(N,EXPR) (regs.regs_F.l[(N)] = (EXPR))
+#define FPR_F(N)    (regs.regs_F.f[(N)])
+#define SET_FPR_F(N,EXPR) (regs.regs_F.f[(N)] = (EXPR))
+#define FPR_D(N)    (regs.regs_F.d[(N) >> 1])
+#define SET_FPR_D(N,EXPR) (regs.regs_F.d[(N) >> 1] = (EXPR))
 
 /* miscellaneous register accessors */
-#define SET_HI(EXPR)		(regs.regs_C.hi = (EXPR))
-#define HI			(regs.regs_C.hi)
-#define SET_LO(EXPR)		(regs.regs_C.lo = (EXPR))
-#define LO			(regs.regs_C.lo)
-#define FCC			(regs.regs_C.fcc)
-#define SET_FCC(EXPR)		(regs.regs_C.fcc = (EXPR))
+#define SET_HI(EXPR)    (regs.regs_C.hi = (EXPR))
+#define HI      (regs.regs_C.hi)
+#define SET_LO(EXPR)    (regs.regs_C.lo = (EXPR))
+#define LO      (regs.regs_C.lo)
+#define FCC     (regs.regs_C.fcc)
+#define SET_FCC(EXPR)   (regs.regs_C.fcc = (EXPR))
 
 #endif
 
 /* precise architected memory state accessor macros */
-#define READ_BYTE(SRC, FAULT)						\
+#define READ_BYTE(SRC, FAULT)           \
   ((FAULT) = md_fault_none, MEM_READ_BYTE(mem, (SRC)))
-#define READ_HALF(SRC, FAULT)						\
+#define READ_HALF(SRC, FAULT)           \
   ((FAULT) = md_fault_none, MEM_READ_HALF(mem, (SRC)))
-#define READ_WORD(SRC, FAULT)						\
+#define READ_WORD(SRC, FAULT)           \
   ((FAULT) = md_fault_none, MEM_READ_WORD(mem, (SRC)))
 #ifdef HOST_HAS_QWORD
-#define READ_QWORD(SRC, FAULT)						\
+#define READ_QWORD(SRC, FAULT)            \
   ((FAULT) = md_fault_none, MEM_READ_QWORD(mem, (SRC)))
 #endif /* HOST_HAS_QWORD */
 
-#define WRITE_BYTE(SRC, DST, FAULT)					\
+#define WRITE_BYTE(SRC, DST, FAULT)         \
   ((FAULT) = md_fault_none, MEM_WRITE_BYTE(mem, (DST), (SRC)))
-#define WRITE_HALF(SRC, DST, FAULT)					\
+#define WRITE_HALF(SRC, DST, FAULT)         \
   ((FAULT) = md_fault_none, MEM_WRITE_HALF(mem, (DST), (SRC)))
-#define WRITE_WORD(SRC, DST, FAULT)					\
+#define WRITE_WORD(SRC, DST, FAULT)         \
   ((FAULT) = md_fault_none, MEM_WRITE_WORD(mem, (DST), (SRC)))
 #ifdef HOST_HAS_QWORD
-#define WRITE_QWORD(SRC, DST, FAULT)					\
+#define WRITE_QWORD(SRC, DST, FAULT)          \
   ((FAULT) = md_fault_none, MEM_WRITE_QWORD(mem, (DST), (SRC)))
 #endif /* HOST_HAS_QWORD */
 
 /* system call handler macro */
-#define SYSCALL(INST)	sys_syscall(&regs, mem_access, mem, INST, TRUE)
+#define SYSCALL(INST) sys_syscall(&regs, mem_access, mem, INST, TRUE)
 
 #ifndef NO_INSN_COUNT
-#define INC_INSN_CTR()	sim_num_insn++
+#define INC_INSN_CTR()  sim_num_insn++
 #else /* !NO_INSN_COUNT */
-#define INC_INSN_CTR()	/* nada */
+#define INC_INSN_CTR()  /* nada */
 #endif /* NO_INSN_COUNT */
 
 
@@ -265,6 +265,11 @@ sim_main(void)
     cycle_count++;
     
     //fprintf(stderr, "\ncycle 15, value1 %d\n", de.oprand.value1 );
+
+    
+    if(cycle_count == 1000){
+      break;
+    }
 
   }
 }
@@ -353,7 +358,7 @@ READ_OPRAND_VALUE:
       
       default:
         break;
-	
+  
     }
     // fprintf(stderr, "\nin1 %d ",de.oprand.in1);
     // fprintf(stderr, "\nin2 %d ",de.oprand.in2);
@@ -372,32 +377,44 @@ READ_OPRAND_VALUE:
     // fprintf(stderr, "\nmw.oprand.out1: %d\n", mw.oprand.out1 );
     if(de.oprand.in1 != -1 && de.opcode > 1 ){ // in1 is needed
       if(de.oprand.in1 == em.oprand.out1 && em.opcode > 1){ // same with em out1
-        de.latched = 1; // stalled
-        de.inst = MD_NOP_INST;
-        de.opcode = NOP;
-        
+        // de.latched = 1; // stalled
+        // de.inst = MD_NOP_INST;
+        // de.opcode = NOP;
+        if(em.opcode != LW){
+          de.oprand.value1 = em.alui;
+          //fprintf(stderr, "\nin1 forwarding from EM: %d\n", de.oprand.value1 );
+        }else{
+          //insert nop
+          de.latched = 1; // stalled
+          de.inst = MD_NOP_INST;
+          de.opcode = NOP;
+        }
         
       }
       if(mw.opcode > 1 && de.oprand.in1 == mw.oprand.out1){
-        
-          de.latched = 1; // stalled
-        de.inst = MD_NOP_INST;
-        de.opcode = NOP;
-        
+        if(mw.opcode == LW){
+          de.oprand.value1 = mw.memload;
+          //fprintf(stderr, "\nin1 forwarding from MW: %d\n", de.oprand.value1 );
+        }
       }
     }
     if(de.oprand.in2 != -1 && de.opcode > 0){
       if(de.oprand.in2 == em.oprand.out1 && em.opcode > 1){
-        
+        if(em.opcode != LW){
+          de.oprand.value2 = em.alui;
+          //fprintf(stderr, "\nin2 forwarding from EM: %d\n", de.oprand.value2 );
+        }else{
           de.latched = 1; // stalled
           de.inst = MD_NOP_INST;
           de.opcode = NOP;
-        
+        }
       }
       if(mw.opcode > 1 && de.oprand.in2 == mw.oprand.out1){
-        de.latched = 1; // stalled
-        de.inst = MD_NOP_INST;
-        de.opcode = NOP;
+        if(mw.opcode == LW){
+          de.oprand.value2 = mw.memload;
+          //fprintf(stderr, "\nin2 forwarding from MW: %d\n", de.oprand.value2 );
+
+        }
       }
     }
 }
